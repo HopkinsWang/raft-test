63c18307d84646b35436ac084688a8b950ef513f
wangqiang <wangqiang@tianhecloud.com>
2022-09-06 11:46:01 +0800

    test for raft grpc

---
+++ ondisk_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	pb "RingAllReduce_29server/ondisk/proto"
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"strings"
+	"time"
+
+	//"RingAllReduce_29server/ondisk"
+
+)
+
+var (
+	hostaddrlist = []string{
+		"192.168.0.232:61001",
+		"192.168.0.232:61002",
+		"192.168.0.232:61003",}
+)
+
+func Ondisktest() {
+	//var clients map[string]pb.RaftdServiceClient
+	clients := make(map[string]pb.RaftdServiceClient,3)
+	for _,addr:=range hostaddrlist{
+		clients[addr]= startTestclient(addr)
+	}
+	for i :=1;i<4;i++{
+		key:= fmt.Sprintf("%v",i)
+		val:= strings.Repeat(key,4)
+		log.Printf("key = %v, val = %v",key,val)
+		_,err:=clients[hostaddrlist[i-1]].Put(context.Background(), &pb.PutRequest{Key:key,Val: val})
+		if err!=nil{
+			log.Printf("Put failed, Err: %v",err)
+		}
+	}
+
+	time.Sleep(3*time.Second)
+	for i :=3;i<0;i--{
+		res, err:=clients[hostaddrlist[i]].Get(context.Background(), &pb.GetRequest{Key:fmt.Sprintf("%v",i)})
+		if err!=nil{
+			log.Printf("clients[hostaddrlist[%v]].Get faile, %v",i,err)
+		}
+		log.Printf("Host: %v  Get! key=%v , val=%v ",hostaddrlist[i],res.Key,res.Val)
+	}
+	log.Printf("Test Finish!")
+
+}
+
+func startTestclient(addr string) pb.RaftdServiceClient{
+	rcv_size := 512 * 1024 * 1024
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(rcv_size), grpc.MaxCallSendMsgSize(rcv_size)))
+	if err != nil {
+		log.Printf(" Net.connect err: %v", err)
+	}
+	return pb.NewRaftdServiceClient(conn)
+}
+
